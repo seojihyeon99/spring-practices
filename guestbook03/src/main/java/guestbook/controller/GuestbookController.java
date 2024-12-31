abff66b22732ac8c303628f681a411abb952bc38
seojihyeon99 <seojihyeon99@naver.com>
2024-12-31 16:47:05 +0900

    241231 guestbook03 JDBC 전략 패턴 적용

--- guestbook03/src/main/java/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/guestbook/controller/GuestbookController.java
@@ -1,15 +1,21 @@
 package guestbook.controller;
 
+import java.util.Enumeration;
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import guestbook.repository.GuestbookRepository;
 import guestbook.vo.GuestbookVo;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class GuestbookController {
@@ -21,7 +27,26 @@ public class GuestbookController {
 	}
 	
 	@RequestMapping("/")
-	public String index(Model model) {
+	public String index(/*HttpServletRequest request,*/ Model model) {
+		/*
+		ServletContext sc = request.getServletContext();
+		Enumeration<String> e = sc.getAttributeNames();
+		while(e.hasMoreElements()) {
+			String name = e.nextElement();
+			System.out.println(name);
+		}
+		
+		ApplicationContext ac1 = (ApplicationContext) sc.getAttribute("org.springframework.web.context.WebApplicationContext.ROOT");
+		ApplicationContext ac2 = (ApplicationContext) sc.getAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.spring");
+		GuestbookRepository repository = ac1.getBean(GuestbookRepository.class);
+		System.out.println(repository);
+		
+		GuestbookController controller = ac2.getBean(GuestbookController.class);
+		System.out.println(controller);
+		
+		System.out.println(ac1 == ac2);
+		*/
+		
 		List<GuestbookVo> list = guestbookRepository.findAll();
 		model.addAttribute("list", list);
 		
@@ -38,14 +63,12 @@ public class GuestbookController {
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
 	public String deleteform(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("id", id);
+		
 		return "delete";
 	}
 	
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.POST)
-	public String delete(GuestbookVo vo) {
-		Long id = vo.getId();
-		String password = vo.getPassword();
-		
+	public String delete(@PathVariable("id") Long id, @RequestParam(value="password", required=true, defaultValue="") String password) {
 		guestbookRepository.deleteByIdAndPassword(id, password);
 		
 		return "redirect:/";
