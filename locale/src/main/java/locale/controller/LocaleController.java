8a3121aa75017ec0bfb3544098111c37cc825bc4
seojihyeon99 <seojihyeon99@naver.com>
2025-01-03 14:52:18 +0900

    250103 Locale

---
+++ locale/src/main/java/locale/controller/LocaleController.java
@@ -0,0 +1,23 @@
+package locale.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class LocaleController {
+	private LocaleResolver localeResolver;
+	
+	public LocaleController(LocaleResolver localeResolver) {
+		this.localeResolver = localeResolver;
+	}
+
+
+	@RequestMapping("/")
+	public String index(HttpServletRequest request) {
+		System.out.println("resolver-locale: " + localeResolver.resolveLocale(request).getLanguage());
+		return "index";
+	}
+}
